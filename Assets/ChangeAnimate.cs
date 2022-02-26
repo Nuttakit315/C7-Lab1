4f114c100ce788466b468e037c6a416776881a99
Nuttakit Hongthong <nuttakithongthong@gmail.com>
2022-02-27 04:17:01 +0700

    Nus

---
+++ Assets/ChangeAnimate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimate : MonoBehaviour
+{
+  public void getAnimatorDefeat(){
+         this.GetComponent<Animator>().Play("Defeat");
+    }
+    public void getgetAnimatorDance(){
+        this.GetComponent<Animator>().Play("Dance");
+    }
+}
